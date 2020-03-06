b707357864e6e35bba62615dc0ae1060f2ccb4fa
Konrad Gałczyński <konrad.galczynski@relativity.com>
2020-03-06 09:25:10 +0100

    initial commit

---
+++ TennisSimplified/TennisScoringSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TennisSimplified
+{
+	public class TennisScoringSystem
+	{
+		public void FirstPlayerScored()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool GetResult()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void SecondPlayerScored()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ TennisSimplifiedSpecification/TennisScoringSystem.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace TennisSimplifiedSpecification
+{
+    public class TennisScoringSystem
+    {
+	    [Fact]
+	    public void SampleTest()
+	    {
+		    Assert.True(true);
+	    }
+    }
+}
